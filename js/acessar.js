a9f0d305560dc86930fae5a6c2bbeb7abcce7e68
hrbraga <hugbraga@gmail.com>
2025-01-07 15:32:02 -0300

    adicionar código de acesso

--- js/acessar.js
+++ js/acessar.js
@@ -14,7 +14,7 @@ function acessar() {
         return;
     }
 
-    const codigosValidos = ["1871"];
+    const codigosValidos = ["1234"];
 
     if (codigosValidos.includes(codigoDigitado)) {
         alert("Acesso Autorizado!");
