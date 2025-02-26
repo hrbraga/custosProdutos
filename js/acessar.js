3f8cee9f9542aff2a1dedd113fd036772dc74c62
hrbraga <hugbraga@gmail.com>
2025-02-26 12:16:50 -0300

    LOJA 5901

--- js/acessar.js
+++ js/acessar.js
@@ -14,7 +14,7 @@ function acessar() {
         return;
     }
 
-    const codigosValidos = ["1871, 5901"];
+    const codigosValidos = ["1871", "5901"];
 
     if (codigosValidos.includes(codigoDigitado)) {
         alert("Acesso Autorizado!");
