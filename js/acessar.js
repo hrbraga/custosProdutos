b1e87646675932ddd3f687b36bf71cd90238e0c0
hrbraga <hugbraga@gmail.com>
2025-02-27 12:49:47 -0300

    1065

--- js/acessar.js
+++ js/acessar.js
@@ -14,7 +14,7 @@ function acessar() {
         return;
     }
 
-    const codigosValidos = ["1871", "5901", "2631", "4183", "7731", "1300"];
+    const codigosValidos = ["1871", "5901", "2631", "4183", "7731", "1300", "1065"];
 
     if (codigosValidos.includes(codigoDigitado)) {
         alert("Acesso Autorizado!");
