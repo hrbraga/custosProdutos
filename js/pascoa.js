65a2b9a7b05f015915aba6e90d4d6191436123e8
hrbraga <hugbraga@gmail.com>
2025-03-12 11:06:38 -0300

    8135

--- js/pascoa.js
+++ js/pascoa.js
@@ -1,4 +1,4 @@
-const usuariosAutorizados = ["1871", "1692", "6890"];
+const usuariosAutorizados = ["1871", "1692", "6890", "8135"];
 
     // Aguarda o carregamento do DOM
     document.addEventListener("DOMContentLoaded", function() {
